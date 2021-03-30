6629ed155617b301d87cd9ce0cca386b7d835ebb
Ujwal-1234 <56074956+Ujwal-1234@users.noreply.github.com>
2021-03-30 12:01:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function loginclick(){
+    document.getElementById('idbform').style.display='block';
+    document.getElementById('Sidformout').style.display='none';
+    document.getElementById('idformout').style.display='block';
+}
+function rmloginclick(){
+    document.getElementById('Sidformout').style.display='none';
+    document.getElementById('idbform').style.display='none';
+    document.getElementById('idformout').style.display='none';
+}
+function rmback(){
+    document.getElementById('idbform').style.display='block';
+    document.getElementById('idformout').style.display='none';
+    document.getElementById('Sidformout').style.display='block';
+}
+function tmenu(){
+    if (document.getElementById('idtopic').style.display != 'block'){
+        document.getElementById('idtopic').style.display='block';
+    }else{
+        document.getElementById('idtopic').style.display='none';
+    }
+}
+function tmenu2(){
+    if (document.getElementById('idtopic2').style.display != 'block'){
+        document.getElementById('idtopic2').style.display='block';
+    }else{
+        document.getElementById('idtopic2').style.display='none';
+    }
+}
